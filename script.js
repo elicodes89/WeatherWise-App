b841f1fc3e19cf3e939ef8958c50494784bfd021
elicodes89 <70531308+elicodes89@users.noreply.github.com>
2020-10-12 16:01:17 +0100

    Alt text not working still

--- script.js
+++ script.js
@@ -44,7 +44,7 @@ function showTemperature(response) {
 
   let iconElement= document.querySelector("#icon");
   iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
-   iconElement.setAttribute("alt", response.data.weather[0].description);
+  iconElement.setAttribute("alt", response.data.weather[0].description);
   let currentTemperature = Math.round(response.data.main.temp);
   let cTemp = `${currentTemperature}`;
   let h4 = document.querySelector("#temperature");
