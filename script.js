72df0c05c1e5eb92527bcfb9d9b7a06aac95813d
elicodes89 <70531308+elicodes89@users.noreply.github.com>
2020-10-15 18:11:45 +0100

    Trying to make the forecast hour to work
    added new function called FORMATHOURS

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 let now = new Date();
 
 let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Saturday"];
@@ -33,6 +35,33 @@ let month = months[now.getMonth()];
 
 h5.innerHTML = `${day} ${month} ${date}, ${hours}:${minutes}`;
 
+function formatDate(timestamp){
+	let date = new Date(timestamp);
+	let hours = date.getHours();
+	if (hours < 10){
+	hours = `0${hours}`;
+}
+
+let minutes = date.getMinutes();
+if (minutes < 10) {
+	minutes = `0${minutes}`;
+}
+
+function formatHours(timestamp){
+	let date = new Date(timestamp);
+	let hours = date.getHours();
+	if (hours < 10){
+	hours = `0${hours}`;
+}
+
+let minutes = date.getMinutes();
+if (minutes < 10) {
+	minutes = `0${minutes}`;
+
+}
+
+return `${hours}:${minutes}`;
+
 function showCity(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-city");
@@ -40,13 +69,14 @@ function showCity(event) {
   h1.innerHTML = `${searchInput.value}`;
   searchCity(searchInput.value);
 }
+
 let locationForm = document.querySelector("#search-form");
 locationForm.addEventListener("submit", showCity);
 
 function showTemperature(response) {
 
 
-	celsiusTemperature = response.data.main.temp;
+celsiusTemperature = response.data.main.temp;
 
   let iconElement= document.querySelector("#icon");
   iconElement.setAttribute("src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
@@ -76,20 +106,19 @@ function showTemperature(response) {
   document.querySelector("#descrip").innerHTML =
     response.data.weather[0].main;
 
- 
 }
 
 function displayForecast(response) {
 	let forecastElement= document.querySelector("#forecast");
-	let forecast = response.data.list[0];
+	let forecast = response.data.list[0];  
 	forecastElement.innerHTML = `
 	<div class="col-2">
 			  <h3>
-			  12:00
+			  ${formatHours(forecast.dt * 1000)}
 			  </h3>
 		<img 
 		src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
-		alt=""
+		
 		/>
 		<div class="weather-forecast-temperature">
 			<strong>${Math.round(forecast.main.temp_max)}°</strong>${Math.round(forecast.main.temp_min)}°
@@ -137,14 +166,10 @@ function displaycelsiusTemperature(event){
 
 }
 
-let celsiusTemperature = null;
-
-
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displaycelsiusTemperature);
+// let celsiusTemperature = null;
+// let currentLocationButton = document.querySelector("#current-location-button");
+// currentLocationButton.addEventListener("click", getCurrentLocation);
+// let fahrenheitLink = document.querySelector("#fahrenheit-link");
+// fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+// let celsiusLink = document.querySelector("#celsius-link");
+// celsiusLink.addEventListener("click", displaycelsiusTemperature);
